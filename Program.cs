560dad3c5b41d12fa95ef9f9bafde433a743a74a
mojoju <matilda.gill@gmail.com>
2021-05-14 13:58:20 +1200

    ffffff

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace proggy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string temp;
+
+            Console.WriteLine("Pick a colour! Blue, Green, Pink, Yellow");
+            temp = Console.ReadLine();
+
+         do {
+
+            if (temp = "Blue") {
+
+                
+            }
+            else if (temp = "Green"){
+
+
+            }
+            else if (temp = "Pink"){
+
+
+            }
+            else if (temp = "Yellow")
+
+
+
+        } (while();)
+
         }
     }
 }
